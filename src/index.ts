378eefb04c105d3db07a010c4ed3ed87019660b6
aaltansa <arsalnaufal2007@gmail.com>
2025-06-28 15:09:37 +0700

    [trss]afdasd

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,14 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json());
 app.use("/api/v1/", v1Route);
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
 app.use(errorHandler)
 
 // app.listen(port, () => {
 //   console.log(`Server is running on http://localhost:${port}`);
 // });
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 export default app;
