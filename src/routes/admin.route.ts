a4b3a520e31729ad699c8596561e173ecc535027
aaltansa <arsalnaufal2007@gmail.com>
2025-06-28 13:34:11 +0700

    [+] Fixing token bug for user and admin

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -3,7 +3,7 @@ import { adminMiddleware } from "../middlewares/admin.middleware";
 import { getAdminController, handleCancelSubscriptionAdmin, handlePauseSubscriptionAdmin } from "../controllers/admin.controller";
 import { getAdminDashboard } from "../controllers/admin.controller";
 import { getAllUsersController } from "../controllers/admin.controller";
-import { handleGetSubscriptions, handleResumeSubscription } from "../controllers/subscription.controller";
+import { handleGetSubscriptions, handleResumeSubscriptionAdmin } from "../controllers/subscription.controller";
 import { handleCreateMealplan } from "../controllers/mealplan.controller";
 
 const router = Router();
@@ -15,7 +15,7 @@ router.get("/user/list", getAllUsersController)
 router.get("/subscription/list", handleGetSubscriptions);
 router.patch("/subscription/pause", handlePauseSubscriptionAdmin);
 router.patch("/subscription/cancel", handleCancelSubscriptionAdmin);
-router.patch("/subscription/resume", handleResumeSubscription);
+router.patch("/subscription/resume", handleResumeSubscriptionAdmin);
 router.post('/mealplan', handleCreateMealplan);   
 
 
--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -2,63 +2,70 @@ import { PrismaClient } from "@prisma/client";
 import { generateRefreshToken, verifyRefreshToken } from "../utils/auth.utils";
 import { createError } from "../exceptions/error.exception";
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const createRefreshToken = async (userId: number, role: string) => {
-     const refreshToken = await prisma.refreshToken.create({
-          data: {
-               userId: userId,
-               token: generateRefreshToken(userId, role),
-               expiredAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-          }
-     })
+  const token = generateRefreshToken(userId, role);
 
-     return refreshToken.token;
-}
+  let data: any = {
+    token,
+    expiredAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  };
+
+  if (role === "ADMIN") {
+    data = { ...data, adminId: userId, userId: null };
+  } else if (role === "USER") {
+    data = { ...data, userId: userId, adminId: null };
+  } else {
+    throw createError("failed", "Invalid role for refresh token", 400);
+  }
+
+  const refreshToken = await prisma.refreshToken.create({ data });
+  return refreshToken.token;
+};
 
 export const getToken = async (token: string) => {
-     const refreshToken = await prisma.refreshToken.findFirst({
-          where: {
-               token: token
-          }
-     })
+  const refreshToken = await prisma.refreshToken.findFirst({
+    where: {
+      token: token,
+    },
+  });
 
-     if (!refreshToken) {
-          throw createError("failed", "Token Not Found", 400)
-     }
+  if (!refreshToken) {
+    throw createError("failed", "Token Not Found", 400);
+  }
 
-     return refreshToken
-}
+  return refreshToken;
+};
 
 export const deleteToken = async (token: string) => {
-     if(!await getToken(token)) {
-          throw createError("failed", "Logout failed, token invalid", 400)
-     }
+  if (!(await getToken(token))) {
+    throw createError("failed", "Logout failed, token invalid", 400);
+  }
 
-     return prisma.refreshToken.delete({
-          where: {
-               token
-          }
-     })
-}
+  return prisma.refreshToken.delete({
+    where: {
+      token,
+    },
+  });
+};
 
 export const validateRefreshToken = async (token: string) => {
-     if(!token) {
-          throw createError("failed", "Refresh Token Required", 400)
-     }
+  if (!token) {
+    throw createError("failed", "Refresh Token Required", 400);
+  }
 
-     if(!verifyRefreshToken(token)) {
-          throw createError("failed", "Refresh Token Invalid", 400)
-     }
+  if (!verifyRefreshToken(token)) {
+    throw createError("failed", "Refresh Token Invalid", 400);
+  }
 
-     const refreshToken = await getToken(token)
+  const refreshToken = await getToken(token);
 
-     if (refreshToken){
-          if (new Date(Date.now()) > refreshToken?.expiredAt) {
-               throw createError("failed", "Refresh Token Expired", 400)
-          }
-     }
+  if (refreshToken) {
+    if (new Date(Date.now()) > refreshToken?.expiredAt) {
+      throw createError("failed", "Refresh Token Expired", 400);
+    }
+  }
 
-     return
-}
+  return;
+};
