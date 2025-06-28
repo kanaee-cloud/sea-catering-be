8ba684c2a68aa5b6a3e0e944d7be479510f00c0a
aaltansa <arsalnaufal2007@gmail.com>
2025-06-28 22:28:33 +0700

    [+] Change Controller User Option for Deployment

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -54,14 +54,13 @@ export const loginController = asyncHandler(
 
     const refreshToken = await createRefreshToken(user.id, user.role);
 
-    res.cookie('refreshToken', refreshToken, {
-        httpOnly: true, 
-        secure: false,
-        sameSite: 'lax',
-        path: "/",
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    }
-)
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true, 
+      sameSite: "none", 
+      path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     res.status(200).json({
       status: "success",
@@ -119,5 +118,5 @@ export const logout = asyncHandler(async (req: Request, res: Response) => {
     message: "Successfully Logged out",
   });
 
-    return
-})
+  return;
+});
