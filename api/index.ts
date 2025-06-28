b0014e8d3f015cc9f88be8a37073148a05a9ccab
aaltansa <arsalnaufal2007@gmail.com>
2025-06-28 21:00:40 +0700

    one wish berhasil deploy

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,9 @@
-import app from "../src/index";
+import { PrismaClient } from '@prisma/client';
+import app from '../src/index';
+
+const prisma = new PrismaClient();
+
+
+app.set('prisma', prisma);
 
 export default app;
